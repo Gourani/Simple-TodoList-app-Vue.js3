800b538429d24f32a76cefdf934bf7125458f870
youssef gourani <ygourani@afdtech.com>
2024-02-21 22:09:25 +0100

    first squelette

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Banner from './components/Banner.vue'
+</script>
+<script>
+export default {
+  data(){
+    return{
+      item:"items"
+    }
+  }
+}
+
+</script>
+<template>
+
+    <Banner />
+    <h2>Create a todo</h2>
+    <span>What's on your todo list ?</span>
+    <span>e.g. make a video</span>
+    <span>pick a category</span>
+    <input type="radio" name="category" id="business"  />Business
+    <input type="radio" name="category" id="personal"  />Personal
+    <button>
+      Add todo
+    </button>
+    <h3>Todo List</h3>
+    <div >
+      <input type="radio" name="category" id="busi_or_perso"/>
+      {{ item }}
+      <button>
+        Delete
+      </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+                <p>Message is: {{ message }}</p>
+                <input v-model="message" placeholder="edit me" />
+</template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        message : ""
+      };
+    },
+    mounted() {
+    if (localStorage.message) {
+      this.message = localStorage.message;
+    }
+  },
+  watch: {
+    message(newName) {
+      localStorage.message = newName;
+    }
+  }
+  };
+  </script>
+  
+  <style scoped>
+  span {
+    color: blue;
+  }
+  </style>
+  
